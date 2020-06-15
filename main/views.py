43ceb77dac38fa8d7403daf6a0a1592358a5142a
Qiqi Zhou <qiqizhou@Qiqis-MBP.hsd1.ca.comcast.net>
2020-06-15 00:54:01 -0700

    fixed bugs

--- main/views.py
+++ main/views.py
@@ -9,3 +9,15 @@ def index(response):
 def home(response):
 	return render(response, "main/home.html", {})
 
+def contact(response):
+	return render(response, "main/contact.html", {})
+
+def industry(response):
+	return render(response, "main/industry.html", {})
+
+def teams(response):
+	return render(response, "main/teams.html", {})
+
+def token(response):
+	return render(response, "main/token.html", {})
+
--- visionx/settings.py
+++ visionx/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
     'register.apps.RegisterConfig',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
